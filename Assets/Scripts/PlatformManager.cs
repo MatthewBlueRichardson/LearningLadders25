d7220820ab8708a7a2cdeca622f6d9fc03c8bdfd
NikolaosComandariu <s283743@uos.ac.uk>
2025-07-15 14:22:05 +0100

    Set up basic spawner for test purposes
    I set up a basic spawner for testing purposes. This should be removed after testing.

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -1,8 +1,25 @@
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlatformManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] fallingObjects;
+    private GameObject _lastObjectSpawned;
 
-    
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S)) // TODO: remove after testing!
+        {
+            SpawnRandomObject();
+        }
+    }
+
+    /// <summary>
+    /// This is placeholder code until the spawner is in.
+    /// </summary>
+    private void SpawnRandomObject() // TODO: remove after testing!
+    {
+        // Spawn random object from fallingObjects array.
+        _lastObjectSpawned = Instantiate(fallingObjects[Random.Range(0, fallingObjects.Length)], new Vector3(0, 0, 0), Quaternion.identity);
+    }
 }
