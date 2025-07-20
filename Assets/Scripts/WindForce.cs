b64dcba757070f20ee8c4ed9434883af71491f98
NikolaosComandariu <s283743@uos.ac.uk>
2025-07-20 15:33:01 +0100

    Add set wind functions and trigger them at a random interval

--- Assets/Scripts/WindForce.cs
+++ Assets/Scripts/WindForce.cs
@@ -3,16 +3,58 @@ using UnityEngine;
 
 public class WindForce : MonoBehaviour
 {
-    private AreaEffector2D _effector;
-    private string _windDirection;
+    private AreaEffector2D effector;
+    private string windDirection;
+    private string windForce;
+    private bool isRight;
+    private float timer;
+    private float interval;
 
     private void Awake()
     {
-        _effector = GetComponent<AreaEffector2D>();
+        effector = GetComponent<AreaEffector2D>();
+    }
+
+    private void Start()
+    {
+        SetWindForceToLeft();
+        interval = 10f;
     }
 
     private void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if (isRight && timer >= interval)
+        {
+            SetWindForceToLeft();
+            timer = 0;
+            interval = Random.Range(8, 15);
+        }
+
+        if (!isRight && timer >= interval) 
+        {
+            SetWindForceToRight();
+            timer = 0;
+            interval = Random.Range(8, 15);
+        }
+    }
+
+    private void SetWindForceToLeft()
+    {
+        windDirection = "Wind Direction: West!";
+        effector.forceAngle = 180; // Set to left
+        windForce = effector.forceMagnitude.ToString() + " mph!";
+        isRight = false;
+        Debug.Log("Setting wind to left!" + windDirection + windForce);
+    }
+
+    private void SetWindForceToRight()
+    {
+        windDirection = "Wind Direction: East!";
+        effector.forceAngle = 0; // Set to left
+        windForce = "Wind Force: " + effector.forceMagnitude.ToString() + " mph!";
+        isRight = true;
+        Debug.Log("Setting wind to right!" + windDirection + windForce);
     }
 }
