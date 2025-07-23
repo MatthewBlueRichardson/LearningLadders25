fcb465488ab92c95a5c364a51e785914f8254e17
NikolaosComandariu <s283743@uos.ac.uk>
2025-07-23 14:30:34 +0100

    Finish background object spawner

--- Assets/Scripts/BackgroundObjects.cs
+++ Assets/Scripts/BackgroundObjects.cs
@@ -3,17 +3,19 @@ using UnityEngine;
 public class BackgroundObjects : MonoBehaviour
 {
     [Header("Spawn Positions")]
-    [SerializeField] private Vector3[] leftSpawnPos;
-    [SerializeField] private Vector3[] rightSpawnPos;
+    [SerializeField] private RectTransform[] leftSpawnPos;
+    [SerializeField] private RectTransform[] rightSpawnPos;
 
     [Header("Flying Objects Prefabs")]
     [SerializeField] private GameObject[] lowTierObjects; // First objects that can spawn randomly at low level heights
     [SerializeField] private GameObject[] midTierObjects; // Objects that can spawn at a medium height (could be a range of 5 - 10m)
     [SerializeField] private GameObject[] highTierObjects; // Objects that can spawn at a tall height (could be 10+m).
 
-    private float spawnTimer;
-    private float spawnInterval;
+    [Header("Spawn Variables")]
+    [SerializeField] private float spawnInterval;
+    [SerializeField] private float speed;
 
+    private float spawnTimer = 0; 
     private int currentTier = 0; // 0 = low, 1 = mid, 2 = high
 
     private Vector3 startPos;
@@ -21,30 +23,42 @@ public class BackgroundObjects : MonoBehaviour
 
     private GameObject spawnedObject;
 
+    private bool canMove = false;
+    private float calculatedSpeed;
+
     private void Update()
     {
         spawnTimer += Time.deltaTime;
+        calculatedSpeed = speed * Time.deltaTime;
 
         if (spawnTimer >= spawnInterval)
         {
             SpawnObject();
             spawnTimer = 0f;
         }
+
+         MoveObject();
     }
 
+    /// <summary>
+    /// Decide if the object spawns left or right, check what the current tier is.
+    /// Spawn the appropriate object at the start position and set canMove to true.
+    /// </summary>
     private void SpawnObject()
     {
+        Destroy(spawnedObject);
+        canMove = false;
         int isLeft = Random.Range(0, 2);
 
         if(isLeft == 0) // 0 = object spawns left, 1 = object spawns right.
         {
-            startPos = leftSpawnPos[Random.Range(0, leftSpawnPos.Length)];
-            endPos = rightSpawnPos[Random.Range(0, rightSpawnPos.Length)];
+            startPos = leftSpawnPos[Random.Range(0, leftSpawnPos.Length)].position;
+            endPos = rightSpawnPos[Random.Range(0, rightSpawnPos.Length)].position;
         }
         else
         {
-            startPos = rightSpawnPos[Random.Range(0, rightSpawnPos.Length)];
-            endPos = leftSpawnPos[Random.Range(0, leftSpawnPos.Length)];
+            startPos = rightSpawnPos[Random.Range(0, rightSpawnPos.Length)].position;
+            endPos = leftSpawnPos[Random.Range(0, leftSpawnPos.Length)].position;
         }
 
         switch(currentTier)
@@ -62,20 +76,26 @@ public class BackgroundObjects : MonoBehaviour
                 break;
         }
 
-        // Set path of object to end pos!
-        MoveObject();
+        canMove = true;
     }
 
+    /// <summary>
+    /// Change current tier of background objects.
+    /// </summary>
+    /// <param name="tier"></param>
     public void ChangeTier(int tier)
     {
         currentTier += tier;
-        Debug.Log("New tier of objects! " + currentTier);
         if (currentTier <= 0) currentTier = 0;
         if(currentTier >= 2) currentTier = 2;
     }
 
+    /// <summary>
+    /// Move background object towards the end position.
+    /// </summary>
     private void MoveObject()
     {
-        //spawnedObject.transform.position = endPos;
+        if(!canMove) return;
+        spawnedObject.transform.position = Vector3.MoveTowards(spawnedObject.transform.position, endPos, calculatedSpeed);
     }
 }
