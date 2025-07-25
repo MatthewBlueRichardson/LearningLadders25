3740a8a43f9cc22225e098278387b60aa9b80b37
NikolaosComandariu <s283743@uos.ac.uk>
2025-07-25 14:27:30 +0100

    Link high score and score texts

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -11,22 +11,26 @@ public class ScoreManager : MonoBehaviour
     [SerializeField] private AudioClipSO scoreSound;
     [SerializeField] private AudioClipSO scoreMilestoneSound;
 
+    [Header("Events")]
     [SerializeField] private IntEvent onReachScore;
 
-    public TMP_Text scoreText;
-    public TMP_Text highscoreText;
+    [Header("Game Objects")]
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text highscoreText;
+    [SerializeField] private TMP_Text gameOverHighScoreText; // TODO: Revisit score manager and refactor it!
+    [SerializeField] private TMP_Text gameOverScoreText;
 
-    int score = 0;
-    int highscore = 0;
-    int highestY = 0;
-    bool t1SoundPlayed = false;
-    bool t2SoundPlayed = false;
-
+    private int score = 0;
+    private int highscore = 0;
+    private int highestY = 0;
+    private bool t1SoundPlayed = false;
+    private bool t2SoundPlayed = false;
 
     void Start()
     {
         scoreText.text = score.ToString();
-        highscoreText.text = highscore.ToString();
+        highscoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
+        gameOverHighScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
     }
 
     public void CheckTowerHeight(int blockY)
@@ -36,7 +40,16 @@ public class ScoreManager : MonoBehaviour
             highestY = blockY;
             score = highestY;
             scoreText.text = score.ToString();
+            gameOverScoreText.text = "Score: " +  score.ToString();
             sfxEvent.Invoke(scoreSound);
+            PlayerPrefs.SetInt("Score", score);
+            if (score > PlayerPrefs.GetInt("HighScore"))
+            {
+                Debug.Log("New high score: " + score);
+                PlayerPrefs.SetInt("HighScore", score);
+                gameOverHighScoreText.text = "High Score: " + PlayerPrefs.GetInt("HighScore");
+            }
+                
         }
 
         // Change tier of background objects, 0 = low, 1 = mid, 2 = high.
