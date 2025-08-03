3e9bd34aeaf6a7561625605a8fbc9f7821f1b21f
Niall <124519240+N1alllll@users.noreply.github.com>
2025-08-03 16:07:34 +0100

    Added rising zone that reduces reputation with rep bar animation

---
+++ Assets/RisingKillbox.cs
@@ -0,0 +1,34 @@
+using LearningLadders.EventSystem;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RisingKillbox : MonoBehaviour
+{
+    [SerializeField] private float riseSpeed = 0.6f;
+
+    [SerializeField] private BoolEvent enterEvent;
+    [SerializeField] private BoolEvent exitEvent;
+
+    [SerializeField] private Image repBar;
+
+    void Update()
+    {
+        transform.Translate(Vector3.up * riseSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            enterEvent.Invoke(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            exitEvent.Invoke(false);
+        }
+    }
+}
