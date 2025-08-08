c67588b035044ff9e9383d24dbc4db946b604ffe
Matt <195450767+MatthewBlueRichardson@users.noreply.github.com>
2025-08-08 12:11:47 +0100

    Add Pisa Fall Anim With SFX
    I've just added the pisa falling animation when the play button is pressed.

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,26 @@
+using LearningLadders.Audio;
+using LearningLadders.EventSystem;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    public Animator pisaAnimation;
+    [SerializeField] private float animationTime = 2f;
+    [SerializeField] private AudioClipSO audioClip;
+    [SerializeField] private AudioClipSOEvent onPlaySfxEvent;
+
+    public void StartGame()
+    {
+        onPlaySfxEvent.Invoke(audioClip);
+        pisaAnimation.SetTrigger("Fall");
+        StartCoroutine(LoadGame());
+    }
+
+    private IEnumerator LoadGame()
+    {
+        yield return new WaitForSeconds(animationTime);
+        SceneManager.LoadScene("MainGame");
+    }
+}
